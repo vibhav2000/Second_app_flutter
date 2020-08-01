b1617357690dad55f1a720c62261e82b9cfb78ef
Vibhav Sharma <vibhav2000@vibhav.sharma2000@gmail.com>
2020-08-02 01:28:48 +0530

    this is first commit

---
+++ lib/ui/home.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+myapp() {
+  var mybody = Container(
+    width: 500,
+    height: 500,
+    alignment: Alignment.bottomCenter,
+    child: Text(
+      'This is my Image',
+      style: TextStyle(
+        color: Colors.blue[200],
+        fontWeight: FontWeight.bold,
+        fontSize: 36,
+      ),
+    ),
+    decoration: BoxDecoration(
+        image: DecorationImage(
+            image: NetworkImage(
+                'https://raw.githubusercontent.com/vibhav2000/flutter_repo/master/gmXcHZ6.jpg'),
+            fit: BoxFit.cover),
+        color: Colors.green,
+        borderRadius: BorderRadius.circular(16.0),
+        border: Border.all(
+          width: 5,
+          color: Colors.grey.shade600,
+        )),
+  );
+
+  var myhome = Scaffold(
+      appBar: AppBar(
+        title: Text('MySecondApp'),
+        backgroundColor: Colors.yellow,
+      ),
+      body: Center(
+        child: mybody,
+      ));
+
+  return MaterialApp(home: myhome);
+}
